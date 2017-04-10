acd150c3df3423b87ae785ac7503e85e95c88296
Keawe <account@keawewong.com>
2017-04-10 19:14:29 -0400

    Created the getReportContributors function. Tested.

--- download_avators.js
+++ download_avators.js
@@ -2,3 +2,29 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+}
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var url = `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`;
+
+  request.get(url)
+    .on('error', (err) => {
+      throw err
+    })
+    .on('response', (response) => {
+      var result = response.statusCode
+      if (result !== 200) {
+        console.log(`Response Code: ${result}`)
+        console.log(`url: ${url}`)
+        cb(`Response Code: ${result}`);
+        return
+      };
+    })
+
+}
+
+getRepoContributors('jquery', 'jquery', (result) => {
+  console.log(result);
+})
