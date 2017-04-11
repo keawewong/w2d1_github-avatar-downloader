ef345ccda3354b52ae417ae98eb7c26733f9459e
Keawe <account@keawewong.com>
2017-04-10 21:33:46 -0400

    Command line argument input complete and tested.

--- download_avators.js
+++ download_avators.js
@@ -3,7 +3,26 @@ var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+var repoOwner = process.argv[2]
+var repoName = process.argv[3]
+
+getRepoContributors(repoOwner, repoName, (results) => {
+
+  results.forEach((result) => {
+    var filename = `./images/${result.login}.jpg`
+    downloadImageByURL(result.avatar_url, filename)
+  })
+
+})
+
+
 function getRepoContributors(repoOwner, repoName, cb) {
+
+  if (!repoOwner || !repoName) {
+    console.log(`Please put in the owner and repo name`)
+    return
+  }
+
   var uName = `keawewong`;
   var uToken = `a258d1f5c78475f82f0c64f8512b76d934999991`;
   var url = `https://${uName}:${uToken}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
@@ -15,12 +34,11 @@ function getRepoContributors(repoOwner, repoName, cb) {
   };
 
   request(options, (err, res, body) => {
-    console.log(`Response Code: ${res.statusCode}`)
     if (err) {
       throw err
     }
     if (res.statusCode !== 200) {
-      console.log(`Response Code: res.statusCode`)
+      console.log(`Response Code: ${res.statusCode}`)
       return
     }
     var json = JSON.parse(body);
@@ -45,11 +63,3 @@ function downloadImageByURL(url, filePath) {
 
 
 }
-
-getRepoContributors('jquery', 'jquery', (results) => {
-  results.forEach((result) => {
-    var filename = `./images/${result.login}.jpg`
-    downloadImageByURL(result.avatar_url, filename)
-  })
-})
-
