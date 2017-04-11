d0a826aeeb949b0cb4cdd5ca0f9849720fb5f4fa
Keawe <account@keawewong.com>
2017-04-10 20:38:18 -0400

    Completed the getRepoContributor function. Printed a list of Avator url

--- download_avators.js
+++ download_avators.js
@@ -3,30 +3,36 @@ var request = require('request');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
-}
-
-function getRepoContributors(repoOwner, repoName, cb) {
   var uName = `keawewong`;
-  var uToken = `a459aac5f07d8680c8b493df89bc435f9977ad43`;
+  var uToken = `a258d1f5c78475f82f0c64f8512b76d934999991`;
   var url = `https://${uName}:${uToken}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
+  var options = {
+    url: url,
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
 
-  request.get(url)
-    .on('error', (err) => {
+  request(options, (err, res, body) => {
+    console.log(`Response Code: ${res.statusCode}`)
+    if (err) {
       throw err
-    })
-    .on('response', (response) => {
-      var result = response.statusCode
-      // if (result !== 200) {
-      // };
-        console.log(`Response Code: ${result}`)
-        console.log(`url: ${url}`)
-        cb(`Response Code: ${result}`);
-        return
-    })
+    }
+    if (res.statusCode !== 200) {
+      console.log(`Response Code: res.statusCode`)
+      return
+    }
+    var json = JSON.parse(body);
+    cb(json);
+  });
 
 }
 
-getRepoContributors('jquery', 'jquery', (result) => {
-  console.log(result);
+getRepoContributors('jquery', 'jquery', (results) => {
+
+  results.forEach((result) => {
+    console.log(result.avatar_url);
+  })
+
+    // console.log(results);
 })
